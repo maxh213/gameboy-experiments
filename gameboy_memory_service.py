3a62fa595fe32a162b3de177f9c0516f4a08bc18
Max Harris <maxharris@maxs-mbp.home>
2023-10-15 13:47:13 +0100

    finish shiny loop

--- gameboy_memory_service.py
+++ gameboy_memory_service.py
@@ -24,7 +24,7 @@ class Gameboy_Memory_Service:
         DVs_ADDRESS = 0xDCF4
 
         # Load the DVs from memory
-        dvs = self.pyboy.get_memory_value(DVs_ADDRESS)
+        dvs = self.get_value_dvs(DVs_ADDRESS, 2)
 
         # Check if the Pokemon is shiny
         is_shiny = self.check_shiny(dvs)
@@ -32,25 +32,117 @@ class Gameboy_Memory_Service:
         return is_shiny
 
     def get_first_party_pokemon_info(self):
-
         # Memory addresses
-        SPECIES_ID_ADDRESS = 0xDCDF
-        LEVEL_ADDRESS = 0xDCFE
-        DVs_ADDRESS = 0xDCF4
+        addresses = {
+            'species_id': 0xDCDF,
+            'held_item': 0xDCE0,
+            'move_1': 0xDCE1,
+            'move_2': 0xDCE2,
+            'move_3': 0xDCE3,
+            'move_4': 0xDCE4,
+            'ot_id': 0xDCE5,
+            'experience': 0xDCE7,
+            'hp_exp': 0xDCEA,
+            'atk_exp': 0xDCEC,
+            'def_exp': 0xDCEE,
+            'spd_exp': 0xDCF0,
+            'spe_exp': 0xDCF2,
+            'dvs': 0xDCF4,
+            'move_1_pp': 0xDCF6,
+            'move_2_pp': 0xDCF7,
+            'move_3_pp': 0xDCF8,
+            'move_4_pp': 0xDCF9,
+            'happiness': 0xDCFA,
+            'pokerus_status': 0xDCFB,
+            'caught_data': 0xDCFC,
+            'level': 0xDCFE,
+            'status': 0xDCFF,
+            'hp': 0xDD01,
+            'max_hp_stat': 0xDD03,
+            'attack_stat': 0xDD05,
+            'defense_stat': 0xDD07,
+            'speed_stat': 0xDD09,
+            'special_attack_stat': 0xDD0B,
+            'special_defense_stat': 0xDD0D
+        }
 
         # Load values from memory
-        species_id = self.pyboy.get_memory_value(SPECIES_ID_ADDRESS)
-        level = self.pyboy.get_memory_value(LEVEL_ADDRESS)
-        dvs = self.pyboy.get_memory_value(DVs_ADDRESS)
-
-        # Deduce gender from the Attack DV
-        attack_dv = (dvs & 0xF000) >> 12
-        gender = 'Male' if attack_dv % 2 == 0 else 'Female'
+        pokemon_info = {key: self.get_value(addr, 2 if '_stat' in key else 1) for key, addr in addresses.items()}
 
         # Get Pokémon species name
-        if (species_id == 158):
+        if (pokemon_info['species_id'] == 158):
             pokemon_species_name = 'Totodile'
-        else: 
-            pokemon_species_name = species_id
+        else:
+            pokemon_species_name = pokemon_info['species_id']  # Perhaps lookup in a species dictionary for better results
 
-        return pokemon_species_name, level, gender
+        pokemon_info['species_name'] = pokemon_species_name
+
+        # Extract DVs by using bitwise operations
+        dv_value = self.get_value_dvs(addresses['dvs'], 2)
+        attack_dv = (dv_value & 0xF000) >> 12
+        defense_dv = (dv_value & 0x0F00) >> 8
+        speed_dv = (dv_value & 0x00F0) >> 4
+        special_dv = dv_value & 0x000F
+
+        # Deduce gender from the Attack DV
+        gender = 'Male' if attack_dv % 2 == 0 else 'Female'
+        pokemon_info['gender'] = gender
+
+        # Update pokemon_info dictionary
+        pokemon_info.update({
+            'attack_dv': attack_dv,
+            'defense_dv': defense_dv,
+            'speed_dv': speed_dv,
+            'special_dv': special_dv,
+        })
+
+        pokemon_info['moves'] = str(pokemon_info['move_1']) + ', ' + str(pokemon_info['move_2']) + ', ' + str(pokemon_info['move_3']) + ', ' + str(pokemon_info['move_4'])
+        return pokemon_info
+
+    def print_first_party_pokemon_info(self, current_attempts):
+        info = self.get_first_party_pokemon_info()
+        is_shiny = self.is_first_party_pokemon_shiny()
+        shiny_attack_pass = False
+        shiny_other_stat_pass = False
+        attack_dv = info['attack_dv']
+        if (attack_dv == 2 or attack_dv == 3 or attack_dv == 6 or attack_dv == 7 or attack_dv == 10 or attack_dv == 11 or attack_dv == 14 or attack_dv == 15):
+            shiny_attack_pass = True
+        if (info['defense_dv'] == 10 and info['speed_dv'] == 10 and info['special_dv'] == 10):
+            shiny_other_stat_pass = True
+
+        print(f"""
+Shiny: {is_shiny},
+Number of resets: {str(current_attempts)},
+Pokemon Species: {info['species_name']}, 
+Level: {info['level']}, 
+Gender: {info['gender']}, 
+Attack IV: {info['attack_dv']},
+Defense IV: {info['defense_dv']},
+Speed IV: {info['speed_dv']},
+Special IV: {info['special_dv']},
+Attack IV shiny pass: {shiny_attack_pass},
+Other stat IV shiny pass: {shiny_other_stat_pass},
+Pokérus Status: {info['pokerus_status']},
+Max HP: {info['max_hp_stat']},
+Attack: {info['attack_stat']},
+Defense: {info['defense_stat']},
+Speed: {info['speed_stat']},
+Special Attack: {info['special_attack_stat']},
+Special Defense: {info['special_defense_stat']}
+""")
+
+    def get_value(self, address, length=1):
+        if length == 1:
+            return self.pyboy.get_memory_value(address)
+        else:
+            low_byte = self.pyboy.get_memory_value(address)
+            high_byte = self.pyboy.get_memory_value(address + 1)
+            return high_byte
+
+    def get_value_dvs(self, address, length=1):
+        if length == 1:
+            return self.pyboy.get_memory_value(address)
+        else:
+            low_byte = self.pyboy.get_memory_value(address)
+            high_byte = self.pyboy.get_memory_value(address + 1)
+            return (high_byte << 8) | low_byte 
--- starter_shiny_hunt.py
+++ starter_shiny_hunt.py
@@ -1,74 +1,125 @@
-# This assumes you have a load state next to the starter you're going to select
-# aaaaaaaadowna(can probs check 1st pokemon slot now)
-
 import pyboy
 from pyboy import PyBoy, WindowEvent
-import time
+import threading
 from gameboy_memory_service import Gameboy_Memory_Service
+import sys
+import random
+import time
 
 # Define your list of inputs
 input_sequence = "AAAAAAAAAAAAA"
 
-# Initialize PyBoy with the ROM file and the save state file
-pyboy = PyBoy('roms/crystal.gbc')
-state = open("roms/crystal.gbc.state", "rb")
-pyboy.load_state(state)
-memory_service = Gameboy_Memory_Service(pyboy)
+# Shared variable and lock for tracking total attempts
+total_attempts = 0
+attempts_lock = threading.Lock()
 
-def shiny_hunt_loop(attempts=1): 
-        
-    # Counter for tracking frames
-    frame_counter = 0
+# Shared variable and lock for tracking whether a shiny has been found
+shiny_found = False
+shiny_found_lock = threading.Lock()
 
-    # Loop through the input sequence
-    for input_command in input_sequence:
-        while frame_counter < 20:
-            pyboy.tick()
-            pyboy.tick()
-            pyboy.tick()
-            pyboy.tick()
-            pyboy.tick()
-            frame_counter += 1
+def shiny_hunt_loop(pyboy, memory_service):
+    global total_attempts  # Access the global total_attempts variable
+    global shiny_found  # Access the global shiny_found variable
+    while True:  # Change loop condition to True
+        with shiny_found_lock:  # Acquire the lock to read shiny_found
+            if shiny_found:  # Check shiny_found within the lock
+                print ("Closing down all threads - shiny found!!!")
+                break  # Exit the loop if shiny_found is True
         
-        # Reset the frame counter
+        # Counter for tracking frames
         frame_counter = 0
 
-        # Press and hold the corresponding button
-        if input_command == 'A':
-            pyboy.send_input(WindowEvent.PRESS_BUTTON_A)
-        elif input_command == 'D':
-            pyboy.send_input(WindowEvent.PRESS_ARROW_DOWN)
+        """
+        Waits for a random number of emulator ticks between min_ticks and max_ticks.
+        
+        Args:
+        - min_ticks (int): The minimum number of ticks to wait.
+        - max_ticks (int): The maximum number of ticks to wait.
+        """
+        min_ticks = 0
+        max_ticks = 9000
+        random.seed(time.time())  # Seed the random number generator with the current time
+        num_ticks = random.randint(min_ticks, max_ticks)
+        for _ in range(num_ticks):
+            pyboy.tick()
 
-        # Release the button after 1 frame to simulate a button press and release
-        pyboy.tick()
-        pyboy.tick()
-        pyboy.tick()
-        if input_command == 'A':
-            pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)
-        elif input_command == 'D':
-            pyboy.send_input(WindowEvent.RELEASE_ARROW_DOWN)
+        # Loop through the input sequence
+        for input_command in input_sequence:
+            while frame_counter < 20:
+                pyboy.tick()
+                pyboy.tick()
+                pyboy.tick()
+                pyboy.tick()
+                pyboy.tick()
+                frame_counter += 1
+            
+            # Reset the frame counter
+            frame_counter = 0
 
-    print ("DONE STARTER INPUT")
-    is_shiny = memory_service.is_first_party_pokemon_shiny()
-    info = memory_service.get_first_party_pokemon_info()
-    print(f"Pokemon Species: {info[0]}, Level: {info[1]}, Gender: {info[2]}, Shiny: {'shiny' if is_shiny else 'not shiny'}")
-    print("Number of resets: " + str(attempts))
+            # Press and hold the corresponding button
+            if input_command == 'A':
+                pyboy.send_input(WindowEvent.PRESS_BUTTON_A)
+            elif input_command == 'D':
+                pyboy.send_input(WindowEvent.PRESS_ARROW_DOWN)
 
-    if not is_shiny:
-        state = open("roms/crystal.gbc.state", "rb")
-        pyboy.load_state(state)
-        attempts = attempts + 1
-        shiny_hunt_loop(attempts)
+            # Release the button after 1 frame to simulate a button press and release
+            pyboy.tick()
+            pyboy.tick()
+            pyboy.tick()
+            if input_command == 'A':
+                pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)
+            elif input_command == 'D':
+                pyboy.send_input(WindowEvent.RELEASE_ARROW_DOWN)
 
-shiny_hunt_loop()
+        is_shiny = memory_service.is_first_party_pokemon_shiny()        
 
-while not pyboy.tick():
-    pass
-pyboy.stop()
+        if not is_shiny:
+            with attempts_lock:  # Acquire the lock to update total_attempts
+                total_attempts += 1
+                current_attempts = total_attempts  # Store the current total for printing
+                memory_service.print_first_party_pokemon_info(current_attempts)
+            state = open(f"roms/crystal.gbc.state", "rb")
+            pyboy.load_state(state)
+        else:
+            with shiny_found_lock:  # Acquire the lock to update shiny_found
+                shiny_found = True  # Set shiny_found to True when a shiny is found
+
+def run_emulator():
+    # Initialize PyBoy with the ROM file and the save state file
+    pyboy = PyBoy(f'roms/crystal.gbc', window_type='headless', hide_window='--quiet' in sys.argv)
+    state = open(f"roms/crystal.gbc.state", "rb")
+    pyboy.load_state(state)
+    memory_service = Gameboy_Memory_Service(pyboy)
+
+    # Start the shiny hunt loop
+    shiny_hunt_loop(pyboy, memory_service)
+
+    pyboy.stop()
+
+def run_emulator_window():
+    # Initialize PyBoy with the ROM file and the save state file
+    pyboy = PyBoy(f'roms/crystal.gbc')
+    state = open(f"roms/crystal.gbc.state", "rb")
+    pyboy.load_state(state)
+    memory_service = Gameboy_Memory_Service(pyboy)
+
+    # Start the shiny hunt loop
+    shiny_hunt_loop(pyboy, memory_service)
+
+    pyboy.stop()
 
 
 
+if __name__ == "__main__":  
+    run_emulator()
+    # run_emulator_window()
 
+    # # Create and start threads for each emulator instance
+    # emulator_threads = [threading.Thread(target=run_emulator) for _ in range(500)]
+    # for thread in emulator_threads:
+    #     thread.start()
 
-
+    # # Wait for all threads to complete
+    # for thread in emulator_threads:
+    #     thread.join()
 
